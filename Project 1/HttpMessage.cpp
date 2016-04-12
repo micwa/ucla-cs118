cb7180dbb2f74d9bb3e81e6fcbbe22104a829ede
Michael Wang <micwa6@yahoo.com>
2016-04-11 22:16:09 -0700

    Write HttpMessage class

---
+++ Project 1/HttpMessage.cpp
@@ -0,0 +1,50 @@
+#include "HttpMessage.h"
+
+using namespace std;
+
+HttpMessage::HttpMessage(const string firstLine)
+    : firstLine_(firstLine)
+{}
+
+string HttpMessage::getHttpVersion() const
+{
+    // parse header line
+}
+
+string HttpMessage::getHeader(const string header) const
+{
+    if (headers_.count(header) > 0)
+        return headers_[header];
+    else
+        return "";
+}
+
+void HttpMessage::setHeader(const string header, const string value)
+{
+    headers_[header] = value;
+}
+
+string HttpMessage::getPayload() const
+{
+    return payload_;
+}
+
+void HttpMessage::setPayload(const string payload)
+{
+    payload_ = payload;
+}
+
+// Returns a string representation of this HttpMessage
+string HttpMessage::toString()
+{
+    const string CRLF = "\r\n";
+    string result;
+
+    result += firstLine_ + CRLF;
+    for (auto it = headers_.begin(); it != headers_.end(); ++it)
+        result += it->first + ": " + it->second + CRLF;
+
+    result += CRLF;
+    result += payload_;
+    return result;
+}
---
+++ Project 1/HttpMessage.h
@@ -0,0 +1,21 @@
+#include <string>
+#include <unordered_map>
+
+class HttpMessage
+{
+private:
+    std::string firstLine_;
+    std::unordered_map<std::string, std::string> headers_;
+    std::string payload_;
+public:
+    HttpMessage(std::string firstLine);
+
+    std::string getHttpVersion() const;
+    std::string getHeader(const std::string header) const;
+    void setHeader(const std::string header, const std::string value);
+    std::string getPayload() const;
+    void setPayload(const std::string payload);
+
+    // Returns a string representation of this HttpMessage
+    std::string toString();
+};
