0afc4b33aff8c28fc7ccc1c16f1722c26bd3f909
Michael Wang <micwa6@yahoo.com>
2016-04-22 21:49:08 -0700

    More tests

--- project1/test.cpp
+++ project1/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "HttpRequest.h"
 #include "HttpResponse.h"
+#include "WebUtil.h"
 
 using namespace std;
 
@@ -9,6 +10,11 @@ int main()
     HttpRequest req("1.1", "www.google.com", "/mail");
     HttpResponse resp("1.1", 200, "some_mail_here");
     cout << req.toString();
-    cout << resp.toString();
+    cout << resp.toString() << endl;
+    cout << "Versions: " << req.getHttpVersion() << " " << resp.getHttpVersion() << endl;
+
+    cout << GetVersionFromLine("HTTP/1.1 200 OK") << endl;
+    cout << GetStatusCodeFromStatusLine("HTTP/1.1 404 not found") << endl;
+    cout << GetPathFromRequestLine("GET /some_path HTTP/1.1") << endl;
     return 0;
 }
