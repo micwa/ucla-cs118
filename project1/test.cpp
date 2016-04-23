ce9d435ca7e8ab0539af7f78ae064eeb8fc75c87
Michael Wang <micwa6@yahoo.com>
2016-04-22 23:36:10 -0700

    More tests

--- project1/test.cpp
+++ project1/test.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
 #include "HttpRequest.h"
 #include "HttpResponse.h"
 #include "WebUtil.h"
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -16,5 +18,11 @@ int main()
     cout << getVersionFromLine("HTTP/1.1 200 OK") << endl;
     cout << getStatusCodeFromStatusLine("HTTP/1.1 404 not found") << endl;
     cout << getPathFromRequestLine("GET /some_path HTTP/1.1") << endl;
+
+    vector<string> lines = { "Host: abc.com\r\n", "Another-header : some value \r\n"};
+    HttpRequest *req2 = makeHttpRequest("1.1", "abc.com", "/foo", lines);
+    cout << req2->toString();
+
+    cout << "--- END" << endl;
     return 0;
 }
