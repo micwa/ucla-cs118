d6c9d89f540ec13c21e954e52074558e7e7ace79
Michael Wang <micwa6@yahoo.com>
2016-04-23 12:32:16 -0700

    Write recvResponse()

--- project1/FileRequest.cpp
+++ project1/FileRequest.cpp
@@ -2,6 +2,7 @@
 #include "HttpRequest.h"
 #include "HttpResponse.h"
 #include "WebUtil.h"
+#include "logerr.h"
 
 using namespace std;
 
@@ -10,12 +11,23 @@ FileRequest::FileRequest(string httpVersion, string host, string path)
       response_(nullptr)
 {
     request_ = new HttpRequest(httpVersion, host, path);
+
+    // Extract filename from path
+    int i = path.size() - 1;
+    while (i >= 0 && path[i] != '/')
+        --i;
+    filename_ = path.substr(i + 1);
 }
 
 FileRequest::~FileRequest()
 {
     delete request_;
     delete response_;
+}
+
+HttpResponse *FileRequest::getResponse() const
+{
+    return response_;
 }
 
 bool FileRequest::sendRequest(int sockfd)
@@ -25,5 +37,39 @@ bool FileRequest::sendRequest(int sockfd)
 
 bool FileRequest::recvResponse(int sockfd)
 {
-    return false;
+    // Receive the first line
+    string firstLine;
+    int res = readline(sockfd, firstLine);
+    if (res == 0 || res == -1)
+        return false;
+
+    string httpVersion = getVersionFromLine(firstLine);   
+    int status = getStatusCodeFromStatusLine(firstLine);
+    if (httpVersion == "" || status == -1)
+        return false;
+    _DEBUG("Received first line: " + firstLine);
+
+    // Receive subsequent lines
+    vector<string> lines;
+    res = readlinesUntilEmpty(sockfd, lines);
+    if (res == 0 || res == -1)
+        return false;
+    _DEBUG("Received more lines: " + to_string(lines.size()));
+
+    // Create HttpResponse from lines read
+    delete response_;
+    response_ = makeHttpResponse(httpVersion, status, "", lines);
+
+    // Receive payload
+    string size;
+    string payload;
+    if (!response_ || !response_->getHeader("content-length", size))
+        return false;
+    if (!recvAll(sockfd, payload, stoi(size)))
+        return false;
+
+    // Set payload; make the user save the file
+    response_->setPayload(payload);
+
+    return true;
 }
--- project1/FileRequest.h
+++ project1/FileRequest.h
@@ -9,11 +9,14 @@ class HttpResponse;
 class FileRequest
 {
 private:
+    std::string filename_;
     HttpRequest *request_;
     HttpResponse *response_;
 public:
     FileRequest(std::string httpVersion, std::string host, std::string path);
     ~FileRequest();
+
+    HttpResponse *getResponse() const;
 
     // Sends an HTTP request for the file associated with this FileRequest.
     // Returns true if the request was sent successfully, and false if not.
@@ -21,7 +24,7 @@ public:
 
     // Receives the HTTP response for the file associated with this FileRequest,
     // and saves the file with a filename extracted from the path.
-    // Returns true if the file was retrieved and saved, and false if not.
+    // Returns true if the file was retrieved successfully, and false if not.
     bool recvResponse(int sockfd);
 };
 
