501471979783da36f8efd203ab43ff421681a9ee
jameskang105 <jameskang105@gmail.com>
2016-04-23 03:51:39 -0700

    Updated web-server.cpp, still not done, have to figure out sync/async mainly

--- project1/web-server.cpp
+++ project1/web-server.cpp
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <sstream>
 
+const int BACKLOG = 20; // convention
+
 int main(int argc, char** argv)
 {
   if (argc != 3)
@@ -18,5 +20,24 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  getaddrinfo(argv[0], argv[1], )
+  struct addrinfo addrhint;
+  struct addrinfo *addrserv;
+
+  memset(&addrhint, 0, sizeof(addrinfo));
+  addrhint.ai_family = AF_INET;
+  addrhint.ai_socktype = SOCK_STREAM;
+
+  if (getaddrinfo(argv[0], argv[1], &addrhint, &addrserv) != 0)
+  {
+    _DEBUG("Error in getaddrinfo");
+  }
+
+  int sfd = socket(addrserv->ai_family, addrserv->ai_socktype, addrserv->ai_protocol);
+
+  // if "Address already in use." is a problem look in 5.3 setsockopt
+  bind(sfd, addrserv->ai_addr, addrserv->ai_addrlen);
+
+  listen(sfd, BACKLOG);
+
+  //accept here
 }
