da9b0a10d8decc8ddee34001535be9b3701ca263
Michael Wang <micwa6@yahoo.com>
2016-04-24 09:51:07 -0700

    Tests for parseUrl()

--- project1/test.cpp
+++ project1/test.cpp
@@ -15,13 +15,26 @@ int main()
     cout << resp.toString() << endl;
     cout << "Versions: " << req.getHttpVersion() << " " << resp.getHttpVersion() << endl;
 
+    // Parsing first lines of HTTP messages
     cout << getVersionFromLine("HTTP/1.1 200 OK") << endl;
     cout << getStatusCodeFromStatusLine("HTTP/1.1 404 not found") << endl;
     cout << getPathFromRequestLine("GET /some_path HTTP/1.1") << endl;
 
+    // makeHttpRequest()
     vector<string> lines = { "Host: abc.com\r\n", "Another-header : some value \r\n"};
     HttpRequest *req2 = makeHttpRequest("1.1", "abc.com", "/foo", lines);
     cout << req2->toString();
+
+    // parseUrl()
+    string host, path;
+    int port;
+    string url1 = "bob.com";
+    string url2 = "http://localhost:8080/foo/bar.html";
+
+    cout << parseUrl(url1, host, port, path);
+    cout << ":" << host << ":" << port << ":" << path << endl;
+    cout << parseUrl(url2, host, port, path);
+    cout << ":" << host << ":" << port << ":" << path << endl;
 
     cout << "--- END" << endl;
     return 0;
