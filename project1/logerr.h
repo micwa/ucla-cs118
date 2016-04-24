d97d25f86b6a7b11962016da60e8cbdf6540c62d
Michael Wang <micwa6@yahoo.com>
2016-04-24 11:11:01 -0700

    Remove _INFO() and use #ifdef

--- project1/logerr.h
+++ project1/logerr.h
@@ -1,6 +1,12 @@
-#define _INFO(msg) std::cout << "[" << __FILE__ << ":" << __LINE__ << "] " << msg << endl;
-#define _DEBUG(msg) std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] " << msg << endl;
+// #define LOGGING_OFF
+
+#ifdef LOGGING_OFF
+#define _DEBUG(msg) (void)(msg)
+#define _ERROR(msg) (void)(msg)
+#else
+#define _DEBUG(msg) std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] " << msg << endl
 #define _ERROR(msg) std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] " \
-    "ERROR: "<< msg << endl;
+    "ERROR: "<< msg << endl
+#endif
 
 #include <iostream>
