2508262b40ac49d66d97d0607fefeabc2d0aa67f
jameskang105 <jameskang105@gmail.com>
2016-04-24 21:30:43 -0700

    2 space indents bad, 4 space indents good

--- project1/web-server.cpp
+++ project1/web-server.cpp
@@ -17,72 +17,72 @@ const int WAIT_USEC = 0;
 
 void response()
 {
-  //accept here, and do file stuff
+    //accept here, and do file stuff
 }
 
 int main(int argc, char** argv)
 {
-  if (argc != 4)
-  {
-    cout << "Invalid number of arguments!\n";
-    return 1;
-  }
-
-  struct addrinfo addrhint;
-  struct addrinfo *addrserv;
-
-  memset(&addrhint, 0, sizeof(addrinfo));
-  addrhint.ai_family = AF_INET;
-  addrhint.ai_socktype = SOCK_STREAM;
-
-  int port_num = stoi(argv[2]);
-
-  if (getaddrinfo(argv[1], port, &addrhint, &addrserv) != 0)
-  {
-    _DEBUG("Error in getaddrinfo");
-  }
-
-  int sfd = socket(addrserv->ai_family, addrserv->ai_socktype, addrserv->ai_protocol);
-
-  // if "Address already in use." is a problem look in 5.3 setsockopt
-  int yes = 1;
-  if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-  {
-    _DEBUG("Error in setsockopt");
-    return 1;
-  }
-
-  bind(sfd, addrserv->ai_addr, addrserv->ai_addrlen);
-
-  if (listen(sfd, BACKLOG) == -1)
-  {
-    _DEBUG("Error in listen");
-    return 1;
-  }
-
-  struct timeval tv;
-  tv.tv_sec = WAIT_SEC;
-  tv.tv_usec = WAIT_USEC;
-
-  fd_set listening_socket;
-  FD_ZERO(&listening_socket);
-  FD_SET(sfd, &listening_socket);
-
-  for (;;)
-  {
-    if (select(sfd+1, &listening_socket, NULL, NULL, &tv) > 0)
+    if (argc != 4)
     {
-      thread connection(response);
-      connection.detach();
+        cout << "Invalid number of arguments!\n";
+        return 1;
     }
-    else
+
+    struct addrinfo addrhint;
+    struct addrinfo *addrserv;
+
+    memset(&addrhint, 0, sizeof(addrinfo));
+    addrhint.ai_family = AF_INET;
+    addrhint.ai_socktype = SOCK_STREAM;
+
+    int port_num = stoi(argv[2]);
+
+    if (getaddrinfo(argv[1], port, &addrhint, &addrserv) != 0)
     {
-      _DEBUG("Nothing received in time specificed in timeval tv");
+        _DEBUG("Error in getaddrinfo");
     }
-  }
+
+    int sfd = socket(addrserv->ai_family, addrserv->ai_socktype, addrserv->ai_protocol);
+
+    // if "Address already in use." is a problem look in 5.3 setsockopt
+    int yes = 1;
+    if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    {
+        _DEBUG("Error in setsockopt");
+        return 1;
+    }
+
+    bind(sfd, addrserv->ai_addr, addrserv->ai_addrlen);
+
+    if (listen(sfd, BACKLOG) == -1)
+    {
+        _DEBUG("Error in listen");
+        return 1;
+    }
+
+    struct timeval tv;
+    tv.tv_sec = WAIT_SEC;
+    tv.tv_usec = WAIT_USEC;
+
+    fd_set listening_socket;
+    FD_ZERO(&listening_socket);
+    FD_SET(sfd, &listening_socket);
+
+    for (;;)
+    {
+        if (select(sfd+1, &listening_socket, NULL, NULL, &tv) > 0)
+        {
+            thread connection(response);
+            connection.detach();
+        }
+        else
+        {
+            _DEBUG("Nothing received in time specificed in timeval tv");
+        }
+    }
 
 
-  return 1; // should never get here
+    return 1; // should never get here
 }
 
 Server
