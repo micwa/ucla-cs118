8a77536348d7c63c18839935fa535edb86157bcc
Michael Wang <micwa6@yahoo.com>
2016-05-05 14:28:47 -0700

    Prevent SIGPIPE from being sent

--- project1/WebUtil.cpp
+++ project1/WebUtil.cpp
@@ -369,7 +369,7 @@ bool sendAll(int sockfd, const string& data)
 
     while (total < data.size())
     {
-        int sent = send(sockfd, buf + total, bytesLeft, 0);
+        int sent = send(sockfd, buf + total, bytesLeft, MSG_NOSIGNAL);
         //_DEBUG("Sent: " + to_string(sent) + " bytes");
         if (sent == -1)
             return false;
