f0e88ec036e6df1a12677a8d968847b524d4c18b
Michael Wang <micwa6@yahoo.com>
2016-06-01 11:38:16 -0700

    Write makePacket_ton() for creating packets

--- project2/TCPutil.cpp
+++ project2/TCPutil.cpp
@@ -17,7 +17,7 @@ int getIpv4(const string& host, int port, struct addrinfo **result)
     return getaddrinfo(host.c_str(), to_string(port).c_str(), &hints, result);
 }
 
-void htonPacket(struct simpleTCP& packet)
+void htonPacket(simpleTCP& packet)
 {
     packet.setSeqNum(htons(packet.getSeqNum()));
     packet.setAckNum(htons(packet.getAckNum()));
@@ -25,10 +25,24 @@ void htonPacket(struct simpleTCP& packet)
     packet.setFlags(htons(packet.getFlags()));
 }
 
-void ntohPacket(struct simpleTCP& packet)
+void ntohPacket(simpleTCP& packet)
 {
     packet.setSeqNum(ntohs(packet.getSeqNum()));
     packet.setAckNum(ntohs(packet.getAckNum()));
     packet.setWindow(ntohs(packet.getWindow()));
     packet.setFlags(ntohs(packet.getFlags()));
+}
+
+simpleTCP makePacket_ton(uint16_t seq_num, uint16_t ack_num, uint16_t window, uint16_t flags,
+                         const char *message, int size)
+{
+    struct simpleHeader header;
+    header.seq_num = seq_num;
+    header.ack_num = ack_num;
+    header.window = window;
+    header.flags = flags;
+
+    simpleTCP packet(header, message, size);
+    htonPacket(packet);
+    return packet;
 }
--- project2/TCPutil.h
+++ project2/TCPutil.h
@@ -1,6 +1,7 @@
 #ifndef TCPutil_h
 #define TCPutil_h
 
+#include <cstdint>
 #include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -10,8 +11,13 @@
 
 int getIpv4(const std::string& host, int port, struct addrinfo **result);
 
-void htonPacket(struct simpleTCP& packet);
+void htonPacket(simpleTCP& packet);
 
-void ntohPacket(struct simpleTCP& packet);
+void ntohPacket(simpleTCP& packet);
+
+// Creates a simpleTCP packet from the given arguments, and calls htonPacket(packet)
+// before returning it.
+simpleTCP makePacket_ton(uint16_t seq_num, uint16_t ack_num, uint16_t window, uint16_t flags,
+                         const char *message, int size);
 
 #endif
