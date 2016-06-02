5af21b9e7b129d5c0d9dbdf0843de30e2424a623
jameskang105 <jameskang105@gmail.com>
2016-06-01 19:29:11 -0700

    Added sendAckPacket

--- project2/TCPutil.cpp
+++ project2/TCPutil.cpp
@@ -1,4 +1,15 @@
+#include <cstdlib>
 #include <cstring>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <netdb.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 
 #include "TCPutil.h"
@@ -46,3 +57,31 @@ simpleTCP makePacket_ton(uint16_t seq_num, uint16_t ack_num, uint16_t window, ui
     htonPacket(packet);
     return packet;
 }
+
+// sends ack packet (for client)
+void sendAckPacket(uint16_t seq_num, uint16_t ack_num, uint16_t window, uint16_t flags,
+                   const char *message, int size, const struct sockaddr server_addr,
+                   socklen_t server_addr_length, simpleTCP recv_packet)
+{
+    ntohPacket(recv_packet);
+    bool retransmission = false;
+    simpleTCP ack_packet = makePacket_ton(seq_num, ack_num, cong_window, F_ACK, "", 0);
+    if (sendto(sockfd, (void *)&ack_packet, sizeof(ack_packet), 0,
+               server_addr, server_addr_length) == -1)
+    {
+        perror("sendto() error in client while sending ACK");
+    }
+    else
+    {
+        if (!retransmission)
+        {
+            cout << "Sending ACK packet " << recv_packet.getAckNum() << endl;
+            retransmission = true;
+        }
+        else
+        {
+            cout << "Sending ACK packet " << recv_packet.getAckNum() << " Retransmission" << endl;
+        }
+    }
+}
+
